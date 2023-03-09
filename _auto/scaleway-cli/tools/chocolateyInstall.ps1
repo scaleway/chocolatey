62226528d5210206fe276880e70478b4e3f48dab
quantumsheep <quantumsheep@users.noreply.github.com>
2023-03-09 00:48:02 +0000

    Apply Choco package update - scaleway-cli

--- _auto/scaleway-cli/tools/chocolateyInstall.ps1
+++ _auto/scaleway-cli/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
-$version = "2.11.1"
+$version = "2.12.0"
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $removeFile = "$toolsDir\scaleway-cli_${version}_windows_386.exe"
 $renameFile = "$toolsDir\scaleway-cli_${version}_windows_amd64.exe"
